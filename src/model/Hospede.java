25ac3394b27f9a0b2416bb453f317a9ae9f31a2a
marrcoso <marcusvinicius.mtt@gmail.com>
2025-04-23 10:53:45 -0300

    primeira commit

---
+++ src/model/Hospede.java
@@ -0,0 +1,43 @@
+
+package model;
+
+import java.util.Date;
+
+
+public class Hospede extends Pessoa{
+    private String cnpj;
+    private String inscricaoEstadual;
+    private String contato;
+
+    public Hospede(String cnpj, String inscricaoEstadual, String contato, int id, String nome, String fone1, String fone2, String email, String cep, String logradouro, String bairro, String cidade, String complemento, Date dataCadastro, String cpf, String rg, String obs, char status) {
+        super(id, nome, fone1, fone2, email, cep, logradouro, bairro, cidade, complemento, dataCadastro, cpf, rg, obs, status);
+        this.cnpj = cnpj;
+        this.inscricaoEstadual = inscricaoEstadual;
+        this.contato = contato;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+    
+}
